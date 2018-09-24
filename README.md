fc761bc858aa1b77a39bee9e678671ecd26eded9
Arka Dutta <arkadutta@VER-BLR-LT1722.local>
2018-09-24 20:25:44 +0530

    Full Working skeleton

---
+++ README.md
@@ -0,0 +1,1 @@
+CREATE  TABLE IF NOT EXISTS `dropwizard_skeleton`.`parts` (`id` INT  AUTO_INCREMENT ,`name` VARCHAR(150) NOT NULL ,`code` VARCHAR(10) NOT NULL, PRIMARY KEY (`id`) )ENGINE = InnoDB;
---
+++ src/main/java/org/arka/database/PartsMapper.java
@@ -0,0 +1,19 @@
+package org.arka.database;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.arka.model.Part;
+import org.skife.jdbi.v2.StatementContext;
+import org.skife.jdbi.v2.tweak.ResultSetMapper;
+
+public class PartsMapper implements ResultSetMapper<Part> {
+  private static final String ID = "id";
+  private static final String NAME = "name";
+  private static final String CODE = "code";
+
+  public Part map(int i, ResultSet resultSet, StatementContext statementContext)
+      throws SQLException {
+    return new Part(resultSet.getInt(ID), resultSet.getString(NAME), resultSet.getString(CODE));
+  }
+}
